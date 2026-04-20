168dc3a6936edca8ab59e2472ec83b9213748e1e
pandeykrishna2010@gmail.com <pandeykrishna2010@gmail.com>
2026-04-21 00:28:39 +0530

    fix: your message here

--- frontend/Hospital_Managment/src/api/authService.js
+++ frontend/Hospital_Managment/src/api/authService.js
@@ -1,7 +1,7 @@
 import axiosInstance from "./axiosInstance";
 
 export const authService = {
-    //Post /api/logjn  //Arvind Pandey...
+    //Post /api/logjn  //Arvind.
     login : (Credentials)=> axiosInstance.post('/Authentication',Credentials), 
 };
  
